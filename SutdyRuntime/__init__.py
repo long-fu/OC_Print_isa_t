b1b7d98cffcf68487c8a792936e9b70c0e15a596
haoshuai <haoshuai9291@163.com>
2021-06-02 16:58:22 +0800

    测试数据展示

---
+++ SutdyRuntime/__init__.py
@@ -0,0 +1,27 @@
+
+def main():
+
+    # iPhone 6
+    # print(bin(0x1a100c80d2d))
+    # print(bin(0x41a100c80d2d))
+    # print(bin(0x45a100c80d2d))
+    
+    # iPhone X
+    # print(bin(0x21a100ae8d2d))
+    # print(bin(0x61a100ae8d2d))
+    # print(bin(0x65a100ae8d2d))
+    
+    # iPhone 11
+    # print(bin(0x100000100848d29))
+    # print(bin(0x300000100848d29))
+    # print(bin(0x300000100848d2d))
+    
+
+    # iPhone 7 Plus
+    print(bin(0x21a100f14d2d))
+    print(bin(0x61a100f14d2d))
+    print(bin(0x65a100f14d2d))
+    
+
+if __name__ == '__main__':
+    main()
